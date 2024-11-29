f163da98ff90ec6577f1247274319b591a7f3a35
ramesh-exalogic <ramesh.kumar@exalogic.co>
2024-11-29 22:15:57 +0530

    scenario2 added the \n inside the string number solution

--- calculator.rb
+++ calculator.rb
@@ -1,15 +1,28 @@
   require 'rspec'
 # add two strings ad input and Addition as out put
   def add_numbers(val)
+  	#puts(val.gsub("\n", ","))
+  	val = val.gsub("\n", ",")
   	result = 0
   	  aa =	val.split(",")
   	  aa.each do |val|
   	   result = result + val.to_i
-      end	
+      end
+      #print(result)	
+      result
   end
+
+ #problem1
 
  # Input ("1,5")
  #output 6 
+
+ #problem2
+
+ #input ("1\n2,4")
+ #output 7
    
 
 puts(add_numbers("1,5"))
+puts(add_numbers("1\n2,4"))
+
