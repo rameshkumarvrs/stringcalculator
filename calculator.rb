2cbbf07963d4232e1b01c67ff9123ee86db33c07
ramesh-exalogic <ramesh.kumar@exalogic.co>
2024-11-29 22:40:43 +0530

    added the logic for removed forward slas and semicolan issue

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,15 @@
   require 'rspec'
 # add two strings ad input and Addition as out put
   def add_numbers(val)
-  	#puts(val.gsub("\n", ","))
-  	val = val.gsub("\n", ",")
+  	val = val.gsub("\n", ",") # this gsub used to remove new line
+  	val1 = val.gsub(/\/+/, " ")# This gsub used to remove forwardslash 
+  	val2 = val1.gsub(";", ",")#this gsub used to remove semicolan
+  	#puts(val2)
   	result = 0
-  	  aa =	val.split(",")
+  	  aa =	val2.split(",")
   	  aa.each do |val|
   	   result = result + val.to_i
       end
-      #print(result)	
       result
   end
 
@@ -21,8 +22,14 @@
 
  #input ("1\n2,4")
  #output 7
+
+ # problem3
+ #input ("//;\n1;4"))
+ #output 5
    
 
 puts(add_numbers("1,5"))
 puts(add_numbers("1\n2,4"))
+puts(add_numbers("//;\n1;4"))
+ 
 
